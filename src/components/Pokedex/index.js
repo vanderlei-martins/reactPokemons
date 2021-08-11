db117252f9a88280a37bc0620d968ef6b94c765f
vanderlei-martins <superlogica.vanderlei@gmail.com>
2021-08-11 06:24:03 -0300

    busca de pokemons

--- src/components/Pokedex/index.js
+++ src/components/Pokedex/index.js
@@ -1,6 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { View, StyleSheet, Text, ActivityIndicator } from "react-native";
+import {
+	View,
+	StyleSheet,
+	Text,
+	ActivityIndicator,
+	TextInput,
+	TouchableHighlight,
+} from "react-native";
 import { BackgroundPokedex, Listpokemons } from "./styles";
 
 import Pokemons from "../Pokemons";
@@ -8,29 +15,57 @@ import Pokemons from "../Pokemons";
 import {
 	getListAllPokemons,
 	getAllPokemonsSelector,
+	pesquisarPokemomSelector,
 } from "../../redux/PokemomAction";
 
 export default function Pokedex() {
-    const [pagina, setPagina] = useState(1);
+	const [pagina, setPagina] = useState(1);
 	const numColumns = 2;
-    const dispatch = useDispatch();
-    const listPokemons = useSelector(getAllPokemonsSelector);
+	const dispatch = useDispatch();
+	const listPokemons = useSelector(getAllPokemonsSelector);
+	const [pokemonsDaBusca, setPokemonsDaBusca] = useState([]);
 
-    useEffect(() => {
-        dispatch(getListAllPokemons(pagina));
-    }, [])
+	useEffect(() => {
+		dispatch(getListAllPokemons(pagina));
+	}, []);
 
-    async function listarMaisPokemons(){
-        let newPage = pagina + 1;
-        await setPagina(newPage);
-        await dispatch(getListAllPokemons(newPage));
-    }
+	async function listarMaisPokemons() {
+		let newPage = pagina + 1;
+		await setPagina(newPage);
+		await dispatch(getListAllPokemons(newPage));
+	}
+
+	function pesquisarPokemons(busca) {
+		if (!busca) {
+			setPokemonsDaBusca([]);
+			return;
+		}
+
+		let pokemonsFiltrados = listPokemons.filter((pokemom) => {
+			let pos = pokemom.name.toLowerCase().indexOf(busca.toLowerCase());
+			return pos != -1;
+		});
+
+		setPokemonsDaBusca(pokemonsFiltrados);
+	}
+
+	function renderizar() {
+		return pokemonsDaBusca.length !== 0 ? pokemonsDaBusca : listPokemons;
+	}
 
 	return (
 		<BackgroundPokedex>
+			<View style={styles.flowRight}>
+				<TextInput
+					style={styles.searchInput}
+					placeholder="Pesquisar"
+					onChangeText={(textoBusca) => pesquisarPokemons(textoBusca)}
+				/>
+			</View>
+
 			<Listpokemons
 				showsVerticalScrollIndicator={false}
-				data={createRows(listPokemons, numColumns)}
+				data={createRows(renderizar(), numColumns)}
 				keyExtractor={(item) => item.id}
 				renderItem={({ item }) => {
 					if (item.empty) {
@@ -73,5 +108,22 @@ const styles = StyleSheet.create({
 		flexBasis: 0,
 		flexGrow: 1,
 		margin: 5,
+	},
+
+	flowRight: {
+		flexDirection: "row",
+		alignItems: "center",
+		alignSelf: "stretch",
+	},
+
+	searchInput: {
+		height: 36,
+		padding: 4,
+		marginRight: 5,
+		flex: 4,
+		fontSize: 18,
+		borderWidth: 1,
+		borderColor: "#8cc53e",
+		color: "#8cc53e",
 	},
 });
--- src/components/Pokedex/styles.js
+++ src/components/Pokedex/styles.js
@@ -1,12 +1,12 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const BackgroundPokedex = styled.View`
-flex: 1;
-align-items: center;
-justify-content: space-around;
+	flex: 1;
+	align-items: center;
+	justify-content: space-around;
 `;
 
 export const Listpokemons = styled.FlatList`
-    margin-top: 15px;
-    width: 90%;
+	margin-top: 15px;
+	width: 90%;
 `;
